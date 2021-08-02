478c844bec9f624001b1c9c9d36edd16c584c113
Brian McPhillips <B.McPhillips89@gmail.com>
2021-08-01 23:31:37 -0700

    File tree setup

--- src/components/SearchPage/ChracterList/CharacterItem/CharacterItem.jsx
+++ src/components/SearchPage/ChracterList/CharacterItem/CharacterItem.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class App extends Component {
+export default class CharacterItem extends Component {
   render() {
     return (
       <div>
@@ -9,4 +9,3 @@ export default class App extends Component {
     )
   }
 }
-
--- src/components/SearchPage/ChracterList/CharacterList.jsx
+++ src/components/SearchPage/ChracterList/CharacterList.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-export default class App extends Component {
+export default class CharacterList extends Component {
   render() {
     return (
       <div>
@@ -9,4 +9,3 @@ export default class App extends Component {
     )
   }
 }
-
