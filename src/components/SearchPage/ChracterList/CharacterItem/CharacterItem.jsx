71ac052dce28b610d8fae2b7c1b61e776667d40b
Brian McPhillips <B.McPhillips89@gmail.com>
2021-08-03 23:27:34 -0700

    Chracters with name, image, and house rendered to page

--- src/components/SearchPage/ChracterList/CharacterItem/CharacterItem.jsx
+++ src/components/SearchPage/ChracterList/CharacterItem/CharacterItem.jsx
@@ -1,10 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class CharacterItem extends Component {
   render() {
+    const { 
+      data: {
+        image,
+        name,
+        house
+      } 
+    } = this.props;
     return (
       <div>
-        Hello World
+        <img src={image} alt={name} /> 
+        <h3>{name}</h3>
+        <h5>{house}</h5>
       </div>
     )
   }
