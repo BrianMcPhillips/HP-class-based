abc284b8bceacfd8e84b0b3688daa5bff9c5b7a6
Brian McPhillips <B.McPhillips89@gmail.com>
2021-08-03 23:17:26 -0700

    Receiving data from API and setting it to state

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,10 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import request from 'superagent';
 
 export default class SearchPage extends Component {
+  state = {
+    characterData: []
+  }
+  componentDidMount = async() => {
+    const data = await request.get('http://hp-api.herokuapp.com/api/characters');
+    console.log(data.body);
+    this.setState({
+      characterData: data.body
+    })
+  }
   render() {
     return (
       <div>
-        Hello World
+        Hello World 
       </div>
     )
   }
