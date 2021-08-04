cde587ecaabbd84c174a76280504661d89b90e09
Brian McPhillips <B.McPhillips89@gmail.com>
2021-08-03 23:37:26 -0700

    Header component rendered with styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import SearchPage from './SearchPage/SearchPage';
+import Header from './Header/Header';
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <SearchPage /> 
       </div>
     )
   }
 }
-
